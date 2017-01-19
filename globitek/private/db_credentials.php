7e490783d9b8604f8c183ef95e83392840ca7a4f
Xephyr826 <xephyr826@gmail.com>
2017-01-18 18:24:02 -0800

    Ignoring db_credentials.php

--- globitek/private/db_credentials.php
+++ globitek/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
   define("DB_SERVER", "localhost");
-  define("DB_USER", "root");
+  define("DB_USER", "globeuser");
   define("DB_PASS", "");
-  define("DB_NAME", "globitek");
+  define("DB_NAME", "globitekweektwo");
 ?>
