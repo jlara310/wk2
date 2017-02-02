db5d75db338aef92ae69d0a5a5f0ce0789c0e87d
Xephyr826 <xephyr826@gmail.com>
2017-02-01 16:52:06 -0800

    Starting work on new.php for states
    Not done yet.

--- globitek/public/staff/states/new.php
+++ globitek/public/staff/states/new.php
@@ -1,15 +1,33 @@
 <?php
 require_once('../../../private/initialize.php');
+
+// Set default values for all variables the page needs.
+$errors = array();
+$state = array(
+  'name' => '',
+  'code' => '',
+  'country_id' => ''
+);
+
 ?>
 <?php $page_title = 'Staff: New State'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 <div id="main-content">
-  <a href="#add_a_url">Back to States List</a><br />
+  <a href="index.php">Back to States List</a><br />
 
   <h1>New State</h1>
 
-  <!-- TODO add form -->
+  <form action="new.php" method="post">
+    State name:<br />
+    <input type="text" name="name" value="<?php echo h($state['name']); ?>" /><br />
+    State code:<br />
+    <input type="text" name="code" value="<?php echo echo h($state['code']); ?>" /><br />
+    Country ID:<br />
+    <input type="text" name="phone" value="<?php echo h($state['country_id']); ?>" /><br />
+    <br />
+    <input type="submit" name="submit" value="Create"  />
+  </form>
 
 </div>
 
